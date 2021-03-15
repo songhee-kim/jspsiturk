774571db96033d87421c6937eae8903d13a974aa
Songhee Kim <sk4036@nyu.edu>
2021-03-15 14:07:37 -0500

    minor change

--- make_stims.py
+++ make_stims.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 verbs = ['run', 'read', 'reach', 'knock', 'build', 'notice']
 
-queryf = pd.read_excel('/Users/songkim/Google Drive/Primary/Projects/VerbVector/query/query_v0.xlsx', index_col='Name')
+#queryf = pd.read_excel('/Users/songkim/Google Drive/Primary/Projects/VerbVector/query/query_v0.xlsx', index_col='Name')
+queryf = pd.read_excel('/Users/songheekim/Google Drive/Primary/Projects/VerbVector/query/query_v0.xlsx', index_col='Name')
 queryf_event = queryf.iloc[-7:]  ## select only the 7 newly added features 
 feature_names = queryf_event.index.tolist()
 query_dict = queryf_event.to_dict(orient='index')
@@ -14,26 +15,26 @@ query_dict = queryf_event.to_dict(orient='index')
 
 
 ## let's build a dict that will be converted into a json object
-for v in verbs: 
+for vb in verbs: 
     stims_list = []
-    for q in query_dict.items():
+    for k, v in query_dict.items():
+        lemma = vb.capitalize()
         stims = dict()
-        attribute = q[0]
+        attribute = k
         print (attribute)
-        relation = q[1]['Relation']
-        query = q[1]['Query']
-        h_ex = q[1]['High Example']
-        h_exp = q[1]['High Explanation']
-        m_ex = q[1]['Med Example']
-        m_exp = q[1]['Medium Explanation']
-        l_ex = q[1]['Low Example']
-        l_exp = q[1]['Low Explanation']
-        #stims['verb'] = v
+        #relation = q[1]['Relation']
+        query = v['Query']
+        h_ex = v['High Example']
+        h_exp = v['High Explanation']
+        m_ex = v['Med Example']
+        m_exp = v['Medium Explanation']
+        l_ex = v['Low Example']
+        l_exp = v['Low Explanation']
         stims['attribute'] = attribute ## 'Boundedness'
-        stims['question'] = ("<h><b> %s </b></h><br><p><b>To what extent do you think this event described " +
-          "by this verb refer to %s? </b></p><p> For comparison, <b> %s  </b> would receive a high rating on " +
+        if 
+        stims['question'] = ("<h><b> %s </b></h><br><p><b> "%s?" </b></p><p> For comparison, <b> %s  </b> would receive a high rating on " +
           'this question, because %s <br><br>In contrast, <b> %s </b> might receive a medium rating, because ' +
-          "%s </p><br><p>Your rating for <b>%s</b>:</p>") %(v, query, h_ex, h_exp, m_ex, m_exp, v)
+          "%s </p><br><p>Your rating for <b>%s</b>:</p>") %(lemma, query, h_ex, h_exp, m_ex, m_exp, v)
         stims_list.append(stims)
     # with open("%s.json" %(v), "w") as outfile:
     #     json.dump(stims_list, outfile)
